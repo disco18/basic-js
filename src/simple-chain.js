a43b6404980975014bf97062bb98f1d85663fd42
Kiryl Behanski <mr.rspanish@mail.ru>
2020-03-27 14:24:27 +0300

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,35 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value == 'undefined') {
+      value = '( )'
+    } else {
+      value = '( ' + value + ' )'
+    }
+    this.chain.push(value)
+    return this
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position == 'number' && position > 0 && position < this.getLength()) {
+      position -= 1
+      this.chain.splice(position, 1)
+      return this
+    } else {
+      this.chain = []
+      throw new Error('invalid position')
+    }
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.map((_, i, arr) => arr.splice(i, 0, arr.pop()))
+    return this
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const result = this.chain.join('~~')
+    this.chain = []
+    return result
   }
 };
 
