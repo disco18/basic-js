efc7d8f0051988f625a5b50f3090bd453dc04ef4
Kiryl Behanski <mr.rspanish@mail.ru>
2020-03-27 14:01:55 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error('not an array')
+    if (!arr.length) return arr
+
+    let transformedArr = []
+    const len = arr.length - 1
+    for (let i = 0; i <= len; i++) {
+        if (arr[i] == '--discard-next') {
+            if (i + 1 <= len) {
+                i++
+            }
+        } else if (arr[i] == '--discard-prev') {
+            if (i - 1 >= 0) {
+                transformedArr.pop()
+            }
+        } else if (arr[i] == '--double-next') {
+            if (i + 1 <= len) {
+                transformedArr.push(arr[i + 1])
+            }
+        } else if (arr[i] == '--double-prev') {
+            if (i - 1 >= 0) {
+                transformedArr.push(arr[i - 1])
+            }
+        } else {
+            transformedArr.push(arr[i])
+        }
+    }
+    return transformedArr
 };
