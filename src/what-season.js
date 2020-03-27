078649f4fdde201dab98816616cba10d3b718e61
Kiryl Behanski <mr.rspanish@mail.ru>
2020-03-27 13:25:08 +0300

    feat: add what-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!'
+  if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date)) throw new Error('fake date instance')
+  const month = date.getMonth()
+  switch (month) {
+    case 0:
+    case 1:
+    case 11:
+      return 'winter'
+    case 2:
+    case 3:
+    case 4:
+      return 'spring'
+    case 5:
+    case 6:
+    case 7:
+      return 'summer'
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn'
+  }
 };
